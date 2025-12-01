d7c2faf810a39087a4789da415d38dfe4f9beb75
Katherine Tensfeldt <ktensfeldt@perseverenow.org>
2025-12-01 13:16:04 -0700

    Adds the conversation title banner into it's own component.

--- client/src/pages/ConversationDetail.jsx
+++ client/src/pages/ConversationDetail.jsx
@@ -4,11 +4,10 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getOneConversation } from '../redux/conversationSlice'
 import Chats from '../components/chat/Chats'
 import ChatTextarea from '../components/chat/ChatTextarea'
-import ModelInstructionsModal from '../components/modals/ModelInstructionsModal'
+import ConvoTitleBanner from '../components/chat/ConvoTitleBanner'
 
 const ConversationDetail = () => {
   const { id } = useParams()
-  const { conversation } = useSelector((state) => state.conversation)
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -20,21 +19,7 @@ const ConversationDetail = () => {
 
   return (
     <>
-      <nav class="bg-secondary border-y border-neutral-content/70 fixed top-16 z-50 w-full">
-          <div class="px-4 py-2 mx-auto">
-              <div class="flex items-center gap-x-3 text-secondary-content">
-                  <span className="">{conversation.title}</span>
-                  <span className="">•</span>
-                  {/* <button className="btn btn-secondary mix-blend-hard-light h-8">Show Model Instructions</button> */}
-                  <button className="btn btn-secondary mix-blend-hard-light h-8" onClick={() => document.getElementById('model-instructions-modal').showModal()}>Show Model Instructions</button>
-
-                  <ModelInstructionsModal modelInstructions={conversation.modelInstructions} />
-                  {/* mix-blend-screen best so far? **WITH THE bg-base-100 instead of btn-secondary*/}
-                  {/* best with btn-secondary: mix-blend-darken, mix-blend-hard-light,  */}
-                  {/* <button className="btn btn-ghost btn-secondary py-1">Show Model Instructions</button> */}
-              </div>
-          </div>
-        </nav>
+      <ConvoTitleBanner />
       <div class="relative h-full bg-base-100 pt-28">
         {/* <div className="bg-amber-400 min-h-[20vh] pt-16 text-black">
           <div className="text-5xl">Bubble BUBBLE</div>
